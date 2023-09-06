2ab2c63854fd448e3792f2129010bf536a7703f8
shin-6-0 <syk6715@naver.com>
2023-09-06 14:00:02 +0900

    helloweb #3

--- helloweb/src/main/java/servlets/HelloServlet.java
+++ helloweb/src/main/java/servlets/HelloServlet.java
@@ -1,30 +1,25 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloServlet
- */
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at:  ").append(request.getContextPath());
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		
+		String name = request.getParameter("name");
+		pw.print("Hello " + name);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
